b2fa53dca971b83e0bafb8f6b2a8ead9c42469d2
niickyds <ntsd1158@gmail.com>
2023-07-31 20:23:50 -0500

    P4 S4  render cards

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
   },
   {
-    name: "Bals Mountains",
+    name: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
   },
   {
@@ -35,6 +35,9 @@ const profileText = document.querySelector(".profile__text");
 const titleInput = document.querySelector("#profile-title-input");
 const textInput = document.querySelector("#profile-text-input");
 const profileFormEdit = profileModal.querySelector(".modal__form");
+const cardList = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector(".card-template").content.firstElementChild;
 
 // Event Handlers
 
@@ -51,6 +54,16 @@ function closePopup() {
   profileModal.classList.remove("modal_opened");
 }
 
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  cardImage.src = data.link;
+  cardTitle.alt = data.name;
+  cardTitle.textContent = data.name;
+  return cardElement;
+}
+
 editProfileBtn.addEventListener("click", function () {
   titleInput.value = profileTitle.textContent;
   textInput.value = profileText.textContent;
@@ -60,3 +73,8 @@ editProfileBtn.addEventListener("click", function () {
 modalCloseBtn.addEventListener("click", closePopup);
 
 profileFormEdit.addEventListener("submit", handleProfileFormEdit);
+
+initialCards.forEach(function (data) {
+  const cardElement = getCardElement(data);
+  cardList.append(cardElement);
+});
