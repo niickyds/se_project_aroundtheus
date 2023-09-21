be1a2a244cb794209d6801ab3106192839face8a
niickyds <ntsd1158@gmail.com>
2023-09-21 11:59:33 -0500

    p8: setup webpack/ create class files

---
+++ src/utils/popup.js
@@ -0,0 +1,21 @@
+export default class Popup {
+  constructor({ popupSelector }) {
+    this._popupElement = docoument.querySelector(popupSelector);
+  }
+
+  open() {
+    //open popup
+  }
+
+  close() {
+    //close popup
+  }
+
+  _handleEscClose() {
+    // esc listen
+  }
+
+  setEventListeners() {
+    // set event listeners
+  }
+}
---
+++ src/utils/popupWithForm.js
@@ -0,0 +1,20 @@
+import Popup from "./popup.js";
+
+class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super({ popupSelector });
+    this._popupForm = this._popupElement.querySelector(".modal__form");
+    this._handleFormSubmit = handleFormSubmit;
+  }
+
+  close() {
+    this._popupForm.reset();
+    super.close();
+  }
+}
+
+// index
+
+const newCardPopup = popupWithForm("card-modal", () => {
+  // handleFormSubmit
+});
