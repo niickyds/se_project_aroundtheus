2384b0ad0ac6b5a47061959ff2424eed9e831312
niickyds <ntsd1158@gmail.com>
2023-09-25 13:47:45 -0500

    P8: initialize classes. begin bug fixes

--- src/utils/popupWithImage.js
+++ src/utils/popupWithImage.js
@@ -4,12 +4,15 @@ export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super({ popupSelector });
     this._popupImage = this._popupElement.querySelector(".preview-image");
+    this._popupImageText = document.querySelector(".modal__preview-text");
   }
 
-  open({ link, name }) {
-    this._popupElement.querySelector(".modal__preview-text").textContent = name;
-    popupImage.src = link;
-    popupImage.alt = name;
+  open(data) {
+    if (data) {
+      this._popupImageText.textContent = data.name;
+      popupImage.alt = data.name;
+      popupImage.src = data.link;
+    }
     super.open();
   }
 }
